76e3172af73f7fb61e562d192ac9683550101936
unknown <you@example.com>
2023-03-02 10:48:04 +0530

    first-code

---
+++ Mind.java
@@ -0,0 +1,9 @@
+public class Mind{
+	public static void main(String[] args){
+		Mind.samarth();
+	}
+	
+	public static void samarth(){
+		System.out.println("mindGoesBlank");
+	}
+}
---
+++ Old.java
@@ -0,0 +1,11 @@
+public class Old {
+	public static void main(String[] args) {
+		Old.life();
+	
+	}
+    public static void life() {
+		System.out.println("oldLifeIsBest");
+	}
+	
+	
+}
