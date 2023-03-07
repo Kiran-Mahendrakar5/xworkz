9a78c4993bd0814c0e4060b58e4ba11b20ba4823
unknown <you@example.com>
2023-03-07 13:17:40 +0530

    string code

---
+++ Litrals.java
@@ -0,0 +1,27 @@
+public class Litrals{
+	public static void main(String[] args) {
+		String animal = "Tiger";
+		String bird = "Peacock";
+		String cities = "Bangalore";
+		String laptop = "Dell";
+		String train = "Janshatabdi";
+		String bike = "Ktm";
+		String car = "Bmw";
+		String mobile = "Iphone";
+		String games = "Cricket";
+		String seaAnimal = "Fish";
+		
+		System.out.println(animal);
+		System.out.println(bird);
+		System.out.println(cities);
+		System.out.println(laptop);
+		System.out.println(train);
+		System.out.println(bike);
+		System.out.println(car);
+		System.out.println(mobile);
+		System.out.println(games);
+		System.out.println(seaAnimal);
+		
+		
+	}
+}
---
+++ Newkeyword.java
@@ -0,0 +1,28 @@
+public class Newkeyword{
+	public static void main(String[] args){
+		String[] animal = new String[5];
+		String[] bird = new String[4];
+		String[] train = new String[3];
+		String[] bike = new String[2];
+		String[] car = new String[6];
+		String[] seaAnimal = new String[7];
+		String[] game = new String[8];
+		String[] mobile =new String[9];
+		String[] cities = new String[10];
+		String[] laptop = new String[12];
+		
+		System.out.println(animal[4]);
+		System.out.println(bird[3]);
+		System.out.println(train[2]);
+		System.out.println(bike[1]);
+		System.out.println(car[5]);
+		System.out.println(seaAnimal[6]);
+		System.out.println(game[7]);
+		System.out.println(mobile[8]);
+		System.out.println(cities[7]);
+		System.out.println(laptop[9]);
+		
+	} 
+	
+	
+}
