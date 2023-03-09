0bf573403980ee649aaa89d8e2d4869a3f8ddc81
unknown <you@example.com>
2023-03-09 09:48:49 +0530

    Truecode

---
+++ Truekeyword.java
@@ -0,0 +1,20 @@
+public class Truekeyword{
+	public static void main(String[] args){
+		String[] animal = new String[7];
+		String[] notinalAnimal = new String[6];
+		System.out.println(animal[5]==notinalAnimal[5]);
+		String[] bird = new String[8];
+		String[] notinalBird = new String[9];
+		System.out.println(bird[4]==notinalBird[4]);
+		String[] SeaAnimal = new String[7];
+		String[] fish = new String[6];
+		System.out.println(SeaAnimal[4]==fish[4]);
+		String[] mobile = new String[7];
+		String[] iphone = new String[6];
+		System.out.println(mobile[3]==iphone[3]);
+		String[] laptop = new String[5];
+		String[] dell = new String[6];
+		System.out.println(laptop[4]==dell[4]);
+		
+	}
+}
---
+++ Trueliterals.java
@@ -0,0 +1,19 @@
+public class Trueliterals{
+	public static void main(String[] args){
+		String animal = "Tiger";
+		String notinalAnimal = "Tiger";
+		System.out.println(animal==notinalAnimal);
+		String bike = "Ktm";
+		String favBike = "Ktm";
+		System.out.println(bike==favBike);
+		String car = "Bmw";
+		String fourWheeler = "Bmw";
+		System.out.println(car==fourWheeler);
+		String mobile = "Iphone";
+		String apppleMobile = "Iphone";
+		System.out.println(mobile==apppleMobile);
+		String sport = "Game";
+		String running = "Game";
+		System.out.println(sport==running);
+	}
+}
