0497f0d3d27e837939a65cdd8fa5283a0da3d200
unknown <you@example.com>
2023-03-27 18:35:10 +0530

    college car fianlkeyword code

---
+++ Car.java
@@ -0,0 +1,64 @@
+public class Car{
+	final String brand = "Benz";
+	final String name  = "C-Class";
+	final byte numberofWheels = 4;
+	final int mirrors = 2;
+	final byte camera = 1;
+	boolean havingPowerSteering;
+	boolean havingsunroof;
+	static byte seatbelts;
+	static byte gearLever;
+	static String colour;
+	static String location;
+	static byte suspension;
+	final String car = "Benz";
+	int price;
+	byte noWiper;
+	int speedometer;
+	String colourOfAirbag;
+	boolean IsDangerous;
+	byte noSparkPlug;
+	byte gaspedal;
+	byte width;
+	long batterywire;
+    int noOfAirBags;
+	float fuelRatio;
+	float mileage;
+	boolean isHavingSunroof;
+	String magwheels;
+	
+	
+	public static void main(String[] args){
+		
+	}
+	
+	public void giveComfortTravel(){
+		
+	}
+	public void makeAccident(){
+		
+	}
+	public void protectFromHeavyRain(){
+		
+	}
+	public void protectFromAir(){
+		
+	}
+	public void cousingAirPollution(){
+		
+	}
+	public void givelook(){
+		
+	}
+	public void giveprotectionFromSunlight(){
+		
+	}
+	public void difficultyTotravelWithoutAirbeg(){
+		
+	}
+	public void watchingTv(){
+		
+	}
+	
+	
+}
---
+++ College.java
@@ -0,0 +1,78 @@
+public class College{
+	static String collegename;
+	static int collegeregisternumber;
+	static byte numberOfRooms;
+	static char firstLetterOfCollege;
+	static boolean collegeHaving1puc2puc;
+	String subbranch;
+	byte noOfWaterFilter;
+	int collegeFees;
+	Long telephoneNumber;
+	String principalName;
+	String smartClasses;
+	String theColourOfCollege;
+	int classroom;
+	int washroom;
+	int numberofFan;
+	static byte numberOfFloors;
+	String laptops;
+	boolean collegeHavingTheater;
+	int karatefee;
+	int swimmingClassFee;
+	int singingClassFee;
+	int dancingClassFee;
+	boolean collegeHavingBoarding;
+	int boardingFee;
+	int noStudentsInBoarding;
+	int noWashroomInBoarding;
+	byte noofRoomsinboarding;
+	String boardingName;
+	byte noofFans;
+	byte noOfdoor;
+	byte desk;
+	String furniturename;
+	int noStudyTable;
+	int noComper;
+	byte hpWiredMouse;
+	byte noSticks;
+	byte nobroomstick;
+	byte noCollegemaids;
+	byte nochairInCollege;
+	byte noBoards;
+	int noofPlantsTrees;
+	byte noOfTeacher;
+	boolean havingNeetcoaching;
+	
+	public static void main(String[] args){
+		
+	}
+	
+	
+	public static void giveEducation(){
+		
+	}
+	public void PreparingForJobs(){
+		
+	}
+	public void provideknowlegde(){
+		
+	}
+	public void giveGoodDiscipline(){
+		
+	}
+	public void changeTheColour(){
+		
+	}
+	public void giveNeetCoaching(){
+		
+	}
+	public void giveJeecoaching(){
+		
+	}
+	public void giveGoodFeature(){
+		
+	}
+	
+	
+	
+}
