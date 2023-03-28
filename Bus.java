58aa069562dbdaaa67ac2dc41edda39e73242ec4
unknown <you@example.com>
2023-03-28 18:56:24 +0530

    bus code

---
+++ Bus.java
@@ -0,0 +1,92 @@
+public class Bus{
+	static String name ="Bharath Benz";
+	static int model = 2000;
+	int engineSpeed;
+	float fuelMass;
+	byte startOfInjection;
+	long weight ;
+	long noOfBolt ;
+	long noOfNut ;
+	float ratio;
+	byte noOfStroke;
+	short power;
+	long serialNumber;
+	double combustionRatio;
+	char symbol;
+	int number;
+	long engineNumber;
+	double percentage;
+	boolean isyours;
+    byte marks;
+	char grade;
+	short tvnumber;
+	byte batteryPercentage;
+	byte noOfStudents;
+	byte noOfWheelsInBus;
+	byte additionWheelsInbus ;
+	byte noOfDoorsInBus;
+	byte age ;
+	byte noOfStickerInbus;
+	byte noOfSteringInBus;
+	byte nosounds;
+	byte noOfSpeaker;
+	byte noOfMirror;
+	
+	public static void main(String[] args){
+		
+	}
+	public static void makesNoisePollution(){
+	
+    }
+
+    public static void makesSmokePollution(){
+	
+    }
+    public static void givesconsumption(){
+	
+    }
+    public static void makeAccident(){
+	
+    } 
+    public static void givesComfortTravel(){
+	
+    }
+    public static void protectFromHeavyRain(){
+	
+    }
+    public static void earnMoney(){
+	
+    }
+    public static void watchingTv(){
+	
+    }
+    public static void travleOnePlaceToAnother(){
+	
+    }
+	public static void protectFromAir(){
+		
+	}	
+	public static void giveStength(){
+	
+    }
+
+    public static void highEfficiency(){
+	
+    }
+	
+    public static void makesHighTemperature(){
+	
+    }
+	
+    public static void makehighPressure(){
+	
+    }
+	
+    public static void usedForDiesel(){
+	
+    }
+	
+    public static void usedForPetrol(){
+	
+    }
+}
