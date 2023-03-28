d62e66f53a1877061543a1fc11beeb4b1e865bf0
unknown <you@example.com>
2023-03-28 09:34:38 +0530

    college school car state and behaviour

---
+++ School.java
@@ -0,0 +1,77 @@
+public class School{
+	static String Schoolname;
+	static int Schoolregisternumber;
+	static byte numberOfRooms;
+	static char firstLetterOfSchool;
+	String subbranch;
+	byte noOfWaterFilter;
+	int schoolFees;
+	Long telephoneNumber;
+	String principalName;
+	String smartClasses;
+	String theColourOfSchool;
+	int classroom;
+	int washroom;
+	int numberofFan;
+	static byte numberOfFloors;
+	String laptops;
+	boolean schoolHavingTheater;
+	int karatefee;
+	int swimmingClassFee;
+	int singingClassFee;
+	int dancingClassFee;
+	boolean schoolHavingBoarding;
+	int boardingFee;
+	int noStudentsInBoarding;
+	int noWashroomInBoarding;
+	byte noofRoomsinboarding;
+	String boardingName;
+	byte noofFans;
+	byte noOfdoor;
+	byte desk;
+	String furniturename;
+	int noStudyTable;
+	int noComper;
+	byte hpWiredMouse;
+	byte noSticks;
+	byte nobroomstick;
+	byte noSchoolmaids;
+	byte nochairInSchool;
+	byte noBoards;
+	int noofPlantsTrees;
+	byte noOfTeacher;
+	boolean havingNeetcoaching;
+	
+	public static void main(String[] args){
+		
+	}
+	
+	
+	public static void giveEducation(){
+		
+	}
+	public void PreparingForJobs(){
+		
+	}
+	public void provideknowlegde(){
+		
+	}
+	public void giveGoodDiscipline(){
+		
+	}
+	public void changeTheColour(){
+		
+	}
+	public void giveNeetCoaching(){
+		
+	}
+	public void giveJeecoaching(){
+		
+	}
+	public void giveGoodFeature(){
+		
+	}
+	
+	
+	
+}
