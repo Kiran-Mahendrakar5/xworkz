c1df83f9789387012a1d33f4f79063ff291dc4f7
unknown <you@example.com>
2023-03-28 16:03:22 +0530

    university enum code

---
+++ University.java
@@ -0,0 +1,101 @@
+public class University{
+	String principle;
+	String college;
+	short universityCode;
+	String classRooms;
+	int noOfDeportments;
+	int fees;
+	int swimmingFees;
+	int tutionFees;
+	int karateFees;
+	boolean isThisTopuniversity;
+	int noOfTables;
+	int noPens;
+	int noBoard;
+	int noChalk;
+	String courseName;
+	boolean feesInsattlements;
+	byte noMaids;
+	byte noWatchmans;
+	byte noGates;
+	byte noWindows;
+	byte noAquguards;
+	int noOfPlants;
+	int noOfTrees;
+	byte noOfnoticeBoard;
+	byte noOfBooksRooms;
+	byte noOfUniformsRooms;
+	int noOfCctvCameras;
+	int noOfClassTeachers;
+	int noOfStudentlist;
+	int noOfModels;
+	
+	
+	
+	public static void main(String[] args){
+		
+	}
+	public void enterenceTest(){
+		
+	}
+	public static void giveEducation(){
+		
+	}
+	public void PreparingForJobs(){
+		
+	}
+	public void provideknowlegde(){
+		
+	}
+	public void giveGoodDiscipline(){
+		
+	}
+	public void changeTheColour(){
+		
+	}
+	public void giveNeetCoaching(){
+		
+	}
+	public void giveJeecoaching(){
+		
+	}
+	public void giveGoodFeature(){
+		
+	}
+	public void goodSpecilitys(){
+		
+	}
+	public void havingComperScienceCourse(){
+		
+	}
+	public void havingMedicalCourse(){
+		
+	}
+	public void havingNursingCourse(){
+		
+	}
+	public void havingChemicalEngineringCourse(){
+		
+	}
+	public void havinglawCourse(){
+		
+	}
+	public void havingPsychologyCourse(){
+		
+	}
+	public void havingMechanicalEngineringCourse(){
+		
+	}
+	public void havingCivilEngineringCourse(){
+		
+	}
+	public void havingSoftwarEngineringCourse(){
+		
+	}
+	public void havingElectricalEngineringCourse(){
+		
+	}
+	public void havingAeronauticalEngineringCourse(){
+		
+	}
+}
