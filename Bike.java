a1a53705e04e9f2a9e2766054acf468d5111e4f6
unknown <you@example.com>
2023-03-28 15:02:11 +0530

    bike enum codes

---
+++ Bike.java
@@ -0,0 +1,56 @@
+public class Bike{
+	public class Car{
+	final String brand = "Rc 200";
+	final String name  = "KTM";
+	final byte numberofWheels = 2;
+	final int mirrors = 2;
+	boolean havingHeadSet;
+	boolean havingHandlebar;
+	final String bike = "Duke";
+	int price;
+	byte nopedal;
+	String colourOfHandle;
+	boolean IsDangerous;
+	byte noWheels;
+	byte cassette;
+	byte width;
+	long batterywire;
+	boolean isHavingSunroof;
+	String magwheels;
+	
+	
+	public static void main(String[] args){
+		
+	}
+	
+	public void giveComfortTravel(){
+		
+	}
+	public void makeAccident(){
+		
+	}
+	public void giveStength(){
+		
+	}
+	public void helpWithEverdayActivities(){
+		
+	}
+	public void notCousingAirPollution(){
+		
+	}
+	public void givelook(){
+		
+	}
+	public void helpBuildMuscle(){
+		
+	}
+	public void pedalingBuildsBones(){
+		
+	}
+	public void eastonTheJoints(){
+		
+	}
+	
+	
+}
+}
